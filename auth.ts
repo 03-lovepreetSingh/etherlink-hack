5e578ba266ceaaa7bd77afa8831486b081d8732c
03-lovepreetSingh <lovepreetsingh9810573475@gmail.com>
2025-08-04 15:53:19 +0530

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,13 +1,12 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 
 interface User {
-    username?: string;
-    email?: string;
-    name?: string;
-    image?: string;
+  username?: string;
+  email?: string;
+  name?: string;
+  image?: string;
 }
-
 
 interface session {
   accessToken?: string;
@@ -20,22 +19,25 @@ interface session {
   };
 }
 
-
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  secret: process.env.AUTH_SECRET || 'your-auth-secret-key-at-least-32-chars-long',
-  providers: [GitHub({
-    clientId: process.env.AUTH_GITHUB_ID || '',
-    clientSecret: process.env.AUTH_GITHUB_SECRET || '',
-    authorization: {
-      params: {
-        scope: "read:user user:email repo admin:repo_hook write:issues write:pull_requests read:pull_requests"
-      }
-    }
-  })],
+  secret:
+    process.env.AUTH_SECRET || "your-auth-secret-key-at-least-32-chars-long",
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID || "",
+      clientSecret: process.env.AUTH_GITHUB_SECRET || "",
+      authorization: {
+        params: {
+          scope:
+            "read:user user:email repo admin:repo_hook write:issues write:pull_requests read:pull_requests",
+        },
+      },
+    }),
+  ],
   callbacks: {
     async session({ session, token }) {
-      ((session).accessToken as session) = token.accessToken as session;
-      ((session.user).username as User) = token.username as User;
+      (session.accessToken as session) = token.accessToken as session;
+      (session.user.username as User) = token.username as User;
       ((session.user as any).email as User) = token.email as User;
       return session;
     },
@@ -49,10 +51,10 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
     async redirect({ url, baseUrl }) {
       // Redirect to /signup after successful authentication
-      if (url === baseUrl+"/Login") {
+      if (url === baseUrl + "/Login") {
         return `${baseUrl}/homepage`;
       }
       return url;
-    }
-  }
+    },
+  },
 });
